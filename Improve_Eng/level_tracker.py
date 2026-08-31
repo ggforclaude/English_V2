49b59057edb8971239490171b602f287bd600a80
ggforclaude <ggulbok2@gmail.com>
2026-09-01 08:51:24 +0900

    Fix Google Sheets environment variable names

--- Improve_Eng/level_tracker.py
+++ Improve_Eng/level_tracker.py
@@ -170,7 +170,7 @@ class LevelTracker:
             return self._svc
         from google.oauth2 import service_account
         from googleapiclient.discovery import build
-        sa_json = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON", "{}")
+        sa_json = os.environ.get("GOOGLE_CREDENTIALS_JSON", "{}")
         creds = service_account.Credentials.from_service_account_info(
             json.loads(sa_json),
             scopes=["https://www.googleapis.com/auth/spreadsheets"],
